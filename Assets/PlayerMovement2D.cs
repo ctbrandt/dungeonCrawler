3cf5c202ff704de0a431b9b215dd0f001bbdd0f4
Clayton Brandt <ctbrandt22@gmail.com>
2019-06-15 23:16:24 -0500

    Initial commit of DungeonCrawler v1 game

---
+++ Assets/PlayerMovement2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    // Reference to Rigidbody component called "rb"
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public Light flashlight;
+
+    public float movementSpeed = 1f;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+        if(horizontalMove < 0){
+            sr.flipX = true;
+        } else if( horizontalMove > 0){
+            sr.flipX = false;
+        }
+
+        if( Input.GetButtonDown("Fire1") ){
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+
+    // We marked this as "fixed" update because
+    // we are using it to alter physics
+    void FixedUpdate()
+    {
+        rb.MovePosition(new Vector2(horizontalMove * Time.fixedDeltaTime * movementSpeed, verticalMove * Time.fixedDeltaTime * movementSpeed) + rb.position);
+    }
+}
