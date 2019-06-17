26a23b66e19e7f0f634d4347ed6c18a9ac1ef793
Clayton Brandt <ctbrandt22@gmail.com>
2019-06-16 20:10:24 -0500

    Created DialogueManager and created narrator text intro

---
+++ Assets/Scripts/OpenExit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class OpenExit : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Sprite openExit, closedExit;
+    public BoxCollider2D triggerBox;
+    bool canOpenDoor;
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButtonDown("Fire2") && canOpenDoor) {
+            Debug.Log("Open door action");
+            sr.sprite = openExit;
+            triggerBox.enabled = false;
+        }
+    }
+
+    public void OnTriggerEnter2D(){
+        canOpenDoor = true;
+    }
+
+    public void OnTriggerExit2D(){
+        canOpenDoor = false;
+    }
+}
