db7c4b812f4f6b792862b3df6a8400c72b378828
Clayton Brandt <ctbrandt22@gmail.com>
2019-06-24 21:41:27 -0500

    Added new chest sprite fab to begin inventory system

---
+++ Assets/OpenChest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Sprite currSprite, nextSprite;
+    bool canInteract;
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButtonDown("Fire2") && canInteract) {
+            Debug.Log("Environment Interaction");
+            if( sr.sprite == currSprite ) {
+                sr.sprite = nextSprite;
+            }
+            else {
+                sr.sprite = currSprite;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(){
+        canInteract = true;
+    }
+
+    public void OnTriggerExit2D(){
+        canInteract = false;
+    }
+}
